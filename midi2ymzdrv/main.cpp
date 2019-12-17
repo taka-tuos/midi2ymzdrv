d1b16feecff57a6d91a3ae5b8beca7e214bd291e
taka-tuos <hvcnes@gmail.com>
2019-12-17 22:56:03 +0900

    [update] ピッチベンド/エクスプレッション対応

--- midi2ymzdrv/main.cpp
+++ midi2ymzdrv/main.cpp
@@ -15,6 +15,74 @@ FILE *output;
 
 UINT evcount = 0;
 
+UINT notefreq[] = {
+  0,  9,  9, 10,  10, 11,   12,  12, 13, 14, 15, 15,
+  16,  17, 18, 19,  21, 22,  23,  25, 26, 28, 29, 31,
+  33,  35, 37,  39,  41, 44,  46, 49, 52, 55, 58, 62,
+  65, 69, 73, 78,  82, 87,  93, 98, 104,  110,  117,  124,
+  131, 139, 147, 156, 165, 175, 185,  196,  208,  220,  233,  247,
+  262, 277, 294, 311, 330, 349, 370,  392,  415,  440,  466,  494,
+  523, 554, 587, 622, 659, 699, 740,  784,  831,  880,  932,  988,
+  1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976,
+  2093, 2218, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951,
+  4186, 4435, 4699, 4978, 5274, 5587, 5920, 6272, 6645, 7040, 7459, 7902,
+  8372, 8870, 9397, 9956, 10548,11175,  11840,  12544
+};
+
+BYTE notelist[16] = {
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff,
+};
+
+BYTE cc_msb[16] = {
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff
+};
+
+BYTE cc_lsb[16] = {
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff,
+  0xff, 0xff, 0xff, 0xff
+};
+
+BYTE vollist[16] = {
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f
+};
+
+BYTE explist[16] = {
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f
+};
+
+BYTE bendrange[16] = {
+  2, 2, 2, 2,
+  2, 2, 2, 2,
+  2, 2, 2, 2,
+  2, 2, 2, 2
+};
+
+BYTE bvollist[16] = {
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f,
+  0x7f, 0x7f, 0x7f, 0x7f
+};
+
+//#define DEBUG_MIDI
+
+#define MIDI_PITCHBEND_MAX 8191
+#define MIDI_PITCHBEND_MIN -8192
+
 void MidiDataRecieved(UINT stat, UINT data1, UINT data2)
 {
 	int ev = stat & 0xf0;
@@ -22,14 +90,66 @@ void MidiDataRecieved(UINT stat, UINT data1, UINT data2)
 
 	if (ev == 0x90 && ch < 3) {
 		if (beginframe < 0) beginframe = frame;
-		//printf("NOTE ON  : ch = %u, note = %u, velocity = %u : %d FRAME\n", stat & 0xf, data1, data2, frame);
+
+		data2 = (data2 / 4 + 96) * vollist[ch] / 127;
+
+		bvollist[ch] = data2;
+
+		data2 = data2 * explist[ch] / 127;
+
+		notelist[ch] = data1;
+
 		fprintf(output, "\t{ DRV_NOTE_ON     , %2d, %3d, %3d, %5d },\n", ch, data1, data2, frame - beginframe);
 		evcount++;
 	}
 
 	if (ev == 0x80 && ch < 3) {
-		//printf("NOTE OFF : ch = %u, note = %u, velocity = %u : %d FRAME\n", stat & 0xf, data1, data2, frame);
 		fprintf(output, "\t{ DRV_NOTE_OFF    , %2d, %3d, %3d, %5d },\n", ch, data1, data2, frame - beginframe);
+		evcount++;
+	}
+
+	if (ev == 0xb0 && ch < 3) {
+		switch (data1) {
+		case 100:
+			cc_lsb[ch] = data2;
+			break;
+		case 101:
+			cc_msb[ch] = data2;
+			break;
+		case 6:
+			if (cc_msb[ch] == 0 && cc_lsb[ch] == 0) bendrange[ch] = data2;
+			break;
+		case 7:
+			vollist[ch] = data2;
+			break;
+		case 11:
+			explist[ch] = data2;
+			fprintf(output, "\t{ DRV_EXP         , %2d, %3d, %3d, %5d },\n", ch, (bvollist[ch] * data2 / 127), 0, frame - beginframe);
+			evcount++;
+			break;
+		}
+	}
+
+	if (ev == 0xe0 && ch < 3) {
+		union { unsigned a : 14; signed b : 14; } bits;
+		bits.a = data1 | data2 << 7;
+		int bend = bits.b;
+		//printf("%d\n", bend);
+		double freq = notefreq[notelist[ch]];
+		if (bend >= 0) {
+			int freqA = notefreq[notelist[ch] + bendrange[ch]];
+			freq = (freq * (MIDI_PITCHBEND_MAX - bend) + freqA * bend) / MIDI_PITCHBEND_MAX;
+		}
+		else {
+			int freqA = notefreq[notelist[ch] - bendrange[ch]];
+			freq = (freq * (MIDI_PITCHBEND_MIN - bend) + freqA * bend) / MIDI_PITCHBEND_MIN;
+		}
+
+		int freqI = freq;
+
+		printf("%d, %d\n", notefreq[notelist[ch]], freqI);
+
+		fprintf(output, "\t{ DRV_BEND        , %2d, %3d, %3d, %5d },\n", ch, (freqI >> 8) & 0xff, freqI & 0xff, frame - beginframe);
 		evcount++;
 	}
 }
@@ -134,6 +254,11 @@ int main(int argc, char *argv[])
 
 	fclose(head);
 
+#ifdef DEBUG_MIDI
+	FILE *_out = output;
+	output = stdout;
+#endif
+
 	fprintf(output, "ymzdrv_song_t ymzdrvsng_%s[] = {\n", savename);
 
 	res = midiInOpen(&midi_in_handle, device_id, (DWORD_PTR)MidiInProc, 0, CALLBACK_FUNCTION);
@@ -158,6 +283,10 @@ int main(int argc, char *argv[])
 
 	fprintf(output, "};\n");
 	fprintf(output, "// %d bytes(6 x %d)\n", evcount * 6, evcount);
+
+#ifdef DEBUG_MIDI
+	output = _out;
+#endif
 
 	fclose(output);
 
